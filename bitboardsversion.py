7c31224b70429586901c9b63d121808b77f466bd
iamygg2 <142033883+iamygg2@users.noreply.github.com>
2025-04-03 12:19:07 +0000

    eee

---
+++ bitboardsversion.py
@@ -0,0 +1,15 @@
+def createBoard():
+        player1Board = [[0 for _ in range(7)] for _ in range(6)]
+        player2Board = [[0 for _ in range(7)] for _ in range(6)]
+
+        
+
+
+def checkWin():
+    pass
+
+def move():
+    pass
+
+def game():
+    pass
