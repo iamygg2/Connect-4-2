9c2bd3e8e266fe9a514201338719a30dadbdabbc
iamygg2 <142033883+iamygg2@users.noreply.github.com>
2025-04-04 09:37:02 +0100

    ee

--- bitboardsversion.py
+++ bitboardsversion.py
@@ -2,9 +2,6 @@ def createBoard():
         player1Board = [[0 for _ in range(7)] for _ in range(6)]
         player2Board = [[0 for _ in range(7)] for _ in range(6)]
 
-        
-
-
 def checkWin():
     pass
 
@@ -13,3 +10,15 @@ def move():
 
 def game():
     pass
+
+
+class Board:
+    def __init__ (self):
+        self.width = 7
+        self.height = 6
+        self.size = self.width * self.height
+        self.player1_board = 0
+        self.player2_board = 0
+
+    def make_move(self):
+        
